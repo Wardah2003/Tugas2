6871d551fdf4f7dd571e19d5d08309d0726f2d99
Wardah2003 <wardahinsani05@gmail.com>
2025-09-25 11:45:31 +0700

    setup awal

---
+++ tugas.php
@@ -0,0 +1,9 @@
+<?php
+// Daftar nama barang & harga dipisah
+$namaBarang = ["Milo", "Energen", "Kopi", "Gula", "Susu"];
+$hargaBarang = [4000, 3000, 6000, 15000, 7000];
+
+echo "<h1>--POLGAN MART--</h1>";
+
+
+
